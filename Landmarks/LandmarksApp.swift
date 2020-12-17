420c0734a50f771aaff0cdf8aa5ae9233e1f8f61
Alexandr Antonenko <a.antonenko@corp.mail.ru>
2020-12-17 20:03:47 +0300

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Antonenko Aleksandr on 17.12.2020.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
