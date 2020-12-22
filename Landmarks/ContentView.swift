e078bda3b360028eb39d5f5aa2c26cc3da55014d
alexaaaant <alexaaaant@gmail.com>
2020-12-22 20:47:28 +0300

    added some styles

--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -12,6 +12,7 @@ struct ContentView: View {
         VStack {
             MapView()
                 .frame(height: 300)
+                .ignoresSafeArea(edges:.top)
             
             CircleImage()
                 .offset(y: -130)
@@ -22,13 +23,22 @@ struct ContentView: View {
                     .font(.title)
                 HStack {
                     Text("Joshua Tree National Park")
-                        .font(.subheadline)
                     Spacer()
                     Text("California")
-                        .font(.subheadline)
                 }
+                .font(.subheadline)
+                .foregroundColor(.secondary)
+                
+                Divider()
+                
+                Text("About Turtle Rock")
+                    .font(.title2)
+                Text("Descriptive text goes here.")
+                
             }
             .padding()
+            
+            Spacer()
         }
     }
 }
