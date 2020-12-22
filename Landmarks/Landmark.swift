9a9fd49b4512c22b1c74924fde75725f03582ff7
alexaaaant <alexaaaant@gmail.com>
2020-12-22 21:36:54 +0300

    added landmark structure

---
+++ Landmarks/Landmark.swift
@@ -0,0 +1,35 @@
+//
+//  Landmark.swift
+//  Landmarks
+//
+//  Created by Antonenko Aleksandr on 22.12.2020.
+//
+
+import Foundation
+import SwiftUI
+import CoreLocation
+
+struct Landmark: Hashable, Codable {
+    var id: Int
+    var name: String
+    var park: String
+    var state: String
+    var description: String
+    
+    private var imageName: String
+    var image: Image {
+        Image(imageName)
+    }
+    
+    private var coordinates: Coordinates
+    var locationCoordinate: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(
+            latitude: coordinates.latitude,
+            longitude: coordinates.logitude)
+    }
+    
+    struct Coordinates: Hashable, Codable {
+        var latitude: Double
+        var logitude: Double
+    }
+}
