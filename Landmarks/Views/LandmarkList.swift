cbdffac091139099067f68c8b70d5f0652683f8c
alexaaaant <alexaaaant@gmail.com>
2020-12-29 22:18:05 +0300

    added landmark list

---
+++ Landmarks/Views/LandmarkList.swift
@@ -0,0 +1,23 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by Antonenko Aleksandr on 29.12.2020.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        List {
+            LandmarkRow(landmark: landmarks[0])
+            LandmarkRow(landmark: landmarks[1])
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
