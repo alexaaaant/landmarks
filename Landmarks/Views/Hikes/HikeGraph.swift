a44798d40b4905fa658ecb1d0a0d5325b5a1dbd6
alexaaaant <alexaaaant@gmail.com>
2021-01-02 14:59:11 +0300

    added ripple animation

--- Landmarks/Views/Hikes/HikeGraph.swift
+++ Landmarks/Views/Hikes/HikeGraph.swift
@@ -5,6 +5,14 @@ Abstract:
 The elevation, heart rate, and pace of a hike plotted on a graph.
 */
 
+extension Animation {
+    static func ripple(index: Int) -> Animation {
+        Animation.spring(dampingFraction: 0.5)
+            .speed(2)
+            .delay(0.03 * Double(index))
+    }
+}
+
 import SwiftUI
 
 struct HikeGraph: View {
@@ -39,6 +47,8 @@ struct HikeGraph: View {
                         range: observation[keyPath: path],
                         overallRange: overallRange)
                         .colorMultiply(color)
+                        .transition(.slide)
+                        .animation(.ripple(index: index))
                 }
                 .offset(x: 0, y: proxy.size.height * heightRatio)
             }
