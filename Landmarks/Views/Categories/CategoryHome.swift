92d3630a3e436e0077d6f344bc4eb7b8d51a5408
alexaaaant <alexaaaant@gmail.com>
2021-01-03 20:47:15 +0300

    display features

--- Landmarks/Views/Categories/CategoryHome.swift
+++ Landmarks/Views/Categories/CategoryHome.swift
@@ -11,11 +11,20 @@ struct CategoryHome: View {
     @EnvironmentObject var modelData: ModelData
     
     var body: some View {
+
         NavigationView {
             List {
+                modelData.features[0].image
+                    .resizable()
+                    .scaledToFill()
+                    .frame(height: 200)
+                    .clipped()
+                    .listRowInsets(EdgeInsets())
+                
                 ForEach(modelData.categories.keys.sorted(), id: \.self) { key in
-                    Text(key)
+                    CategoryRow(categoryName: key, items: modelData.categories[key]!)
                 }
+                .listRowInsets(EdgeInsets())
             }
             .navigationTitle("Feature")
         }
